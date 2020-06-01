Implemented RSI2 strategy and fixed issues with RSI calculation

development/indicators.py
@@ -42,7 +42,6 @@ class Indicators:
             df1 = df[['Date', 'MA']]
             return df1
         elif typ == 1:
-            print("here")
             smoothing=2
             #first calculate the sma to begin the process
             sma=0
@@ -56,16 +55,18 @@ class Indicators:
             sma/=period
             factor=smoothing /(period+1)
             df1=pd.DataFrame(columns=['Date','MA'])
+            j=0
             for i in range(period, l ,1):
-                prev = sma if i == period else np.asscalar(df1.loc[[i-1],['MA']].values)
+                prev = sma if i == period else np.asscalar(df1.loc[[j-1],['MA']].values)
                 if target == 0:
                     ema=np.asscalar(df.loc[[i],['Adj Close']].values) * factor +  prev * (1-factor)
                 elif target == 1:
                     ema=np.asscalar(df.loc[[i],['High']].values) * factor +  prev * (1-factor)
                 elif target == 2:
                     ema=np.asscalar(df.loc[[i],['Low']].values) * factor +  prev * (1-factor)
-                df1.loc[i,'Date']=df.loc[i,'Date']
-                df1.loc[i,'MA']=ema
+                df1.loc[j,'Date']=df.loc[i,'Date']
+                df1.loc[j,'MA']=ema
+                j+=1
             return df1
 
     def rsi(self,df, period=14):
@@ -84,8 +85,11 @@ class Indicators:
                 iloss+=(prevClose-curClose)
         iagain=igain/period
         ialoss=iloss/period
-        irs=iagain/ialoss
-        irsi=100-(100/(1+irs))
+        if ialoss == 0:
+            irsi=100
+        else:    
+            irs=iagain/ialoss
+            irsi=100-(100/(1+irs))
         df1=pd.DataFrame(columns=['Date','RSI','PAG','PAL'])
         df1.loc[0,'Date']=df.loc[period,'Date']
         df1.loc[0,'RSI']=irsi
@@ -102,10 +106,13 @@ class Indicators:
                 cgain=curClose-prevClose
             else:
                 closs=prevClose-curClose
-            again=((df1.loc[j-1,'PAG']*13)+ (cgain))/period
-            aloss=((df1.loc[j-1,'PAL']*13)+ (closs))/period
-            rs=again/aloss
-            rsi=100-(100/(1+rs))
+            again=((df1.loc[j-1,'PAG']*(period-1))+ (cgain))/period
+            aloss=((df1.loc[j-1,'PAL']*(period-1))+ (closs))/period
+            if aloss == 0:
+                rsi = 100
+            else:    
+                rs=again/aloss
+                rsi=100-(100/(1+rs))
             df1.loc[j,'Date']=df.loc[i,'Date']
             df1.loc[j,'RSI']=rsi
             df1.loc[j,'PAG']=again

development/strategy.py
@@ -15,7 +15,7 @@ class Strategies:
         self._stocks=stocks
         self._timeframe=tf
 
-    def TigerSharkMomentumStrategy(self, names, atr=1.0, rvol=1.2):
+    def TigerSharkMomentumStrategy(self, names, atr=1.0, rvol=1.2, rsi2f=2):
         period=15
         """ Looking for a stock that closes very strong within 90% of the range for longs, or very weak at lower 10% of the range.
             Trigger is the high/low of day
@@ -72,8 +72,57 @@ class Strategies:
         pdf.from_file(temp_html,out_file)
         os.remove(temp_html)      
 
+    def RSI2Strategy(self, names, rsi2f=2, atr=1.0, rvol=1.2):
+        period=250
+        obght=100-rsi2f
+        osld=rsi2f
+        print(osld)
+        j=0
+        for stock in names:
+            if stock in self._stocks:
+                print (stock+" is a stock")
+                ins=Instrument(stock, self._dictc["DB_dir"]+"/"+self._dictc["stock_db"])
+            elif stock in self._etfs:
+                print(stock+" is an ETF")
+                ins=Instrument(stock, self._dictc["DB_dir"]+"/"+self._dictc["etf_db"])
+            df=ins.get_values(hlp.timeframes[self._timeframe], period)
+            if df.shape[0] < 200:
+                continue
+            df1 = df[['Date', 'High', 'Low', 'Adj Close', 'Volume']]
+            df2=ins.ind.ma(df1,200,typ=1)
+            df3=ins.ind.rsi(df1,2)
+            l1=df1.shape[0]
+            l2=df2.shape[0]
+            l3=df3.shape[0]
+            ma200=np.asscalar(df2.loc[[l2-1],['MA']].values)
+            rsi2=np.asscalar(df3.loc[[l3-1],['RSI']].values)
+            curClose=np.asscalar(df.loc[[l1-1],['Adj Close']].values)
+            curDate=np.asscalar(df.loc[[l1-1],['Date']].values)
+            dfr=pd.DataFrame(columns=['Date','RSI','MA','Close'])
+            if  curClose > ma200 and rsi2 <= osld:
+                dfr.loc[j,'Date']=curDate
+                dfr.loc[j,'Close']=curClose
+                dfr.loc[j,'MA']=ma200
+                dfr.loc[j,'RSI']=rsi2
+                j+=1
+            elif curClose < ma200 and rsi2 >= obght:
+                dfr.loc[j,'Date']=curDate
+                dfr.loc[j,'Close']=curClose
+                dfr.loc[j,'MA']=ma200
+                dfr.loc[j,'RSI']=rsi2
+                j+=1
+        temp_html="str.html"
+        dfr.to_html(temp_html, index=False)
+        out_file="str.pdf"
+        pdf.from_file(temp_html,out_file)
+        os.remove(temp_html)      
+
+
+
+
+
     def TestStrategy(self, names, atr=1.0, rvol=1.2):
-        period=50
+        period=250
         for stock in names:
             if stock in self._stocks:
                 print (stock+" is a stock")
@@ -84,12 +133,13 @@ class Strategies:
             df=ins.get_values(hlp.timeframes[self._timeframe], period)
             df1 = df[['Date', 'High', 'Low', 'Adj Close', 'Volume']]
             #df2=ins.ind.ma(df1, 8, typ=1)
-            df2=ins.ind.rsi(df1)
+            df2=ins.ind.rsi(df1,2)
             print(df2)
 
 
 
     tr_strategies = { 0 : TigerSharkMomentumStrategy,
+                      1 : RSI2Strategy,
                      10 : TestStrategy   }
 
     def run(self, strategy, names, **kwargs):